41eba920977b0e930ebdad2871d32f1f0b529c92
mertkutluca <mertkutluca@gmail.com>
2021-03-03 13:32:38 +0300

    remove main storyboard

---
+++ to-do/AppScene/ToDoList/ToDoListVC.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListVC.swift
+//  to-do
+//
+//  Created by mert.kutluca on 3.03.2021.
+//
+
+import UIKit
+
+final class ToDoListVC: UITableViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationItem.title = "test"
+        self.view.backgroundColor = .red
+    }
+}
